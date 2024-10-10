dd108b940bdf90045a035cb9e17589b390b94655
Warren Harding <warrencharding@yahoo.com>
2024-10-10 13:12:44 -0600

    Add Adam/Adam/OptimizationResult.cs

---
+++ Adam/Adam/OptimizationResult.cs
@@ -0,0 +1,9 @@
+//Copyright Warren Harding 2024.
+namespace EthosWork.Adam
+{
+    public class OptimizationResult
+    {
+        public double[] MinimumPoint { get; set; }
+        public double MinimumValue { get; set; }
+    }
+}
