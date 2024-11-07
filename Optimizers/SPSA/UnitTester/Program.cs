81e02563c57c3ecf5f5405b6a8ea91f6cc5a6b68
Warren Harding <warrencharding@yahoo.com>
2024-11-07 01:26:18 -0700

    Add Optimizers/SPSA/UnitTester/Program.cs

---
+++ Optimizers/SPSA/UnitTester/Program.cs
@@ -0,0 +1,14 @@
+//Copyright Warren Harding 2024.
+using System;
+
+namespace SPSA.Tests
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string report = UnitTests.Run();
+            Console.WriteLine(report);
+        }
+    }
+}
