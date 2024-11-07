628db6bfc8b97828b1153e4dfd5426b8f13fa971
Warren Harding <warrencharding@yahoo.com>
2024-11-07 01:26:02 -0700

    Add Optimizers/NesterovAcceleratedGradient/NesterovAcceleratedGradient/Program.cs

---
+++ Optimizers/NesterovAcceleratedGradient/NesterovAcceleratedGradient/Program.cs
@@ -0,0 +1,13 @@
+//Copyright Warren Harding 2024.
+using System;
+
+namespace MyOptimizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        // Entry point of the optimizer
+        }
+    }
+}
