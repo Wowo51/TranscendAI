7960b8794aabfe25a24a57966c2d52f8a729f1a1
Warren Harding <warrencharding@yahoo.com>
2024-11-07 01:25:46 -0700

    Add Optimizers/Adam/UnitTester/Program.cs

---
+++ Optimizers/Adam/UnitTester/Program.cs
@@ -0,0 +1,14 @@
+//Copyright Warren Harding 2024.
+using System;
+
+namespace AdamOptimizerTestNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string report = UnitTests.Run();
+            Console.WriteLine(report);
+        }
+    }
+}
