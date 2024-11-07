52525e8adfcda1965a5f53047f8841bb2d55d0dd
Warren Harding <warrencharding@yahoo.com>
2024-11-07 01:26:14 -0700

    Add Optimizers/RMSprop/UnitTester/Program.cs

---
+++ Optimizers/RMSprop/UnitTester/Program.cs
@@ -0,0 +1,14 @@
+//Copyright Warren Harding 2024.
+using System;
+
+namespace RMSpropTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string report = UnitTests.Run();
+            Console.WriteLine(report);
+        }
+    }
+}
